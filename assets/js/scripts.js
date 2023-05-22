d54bc5654fae35f7918f06387a0f521600dd8b68
Camila Landaeta Rivas <113075041+clr95@users.noreply.github.com>
2023-05-21 23:31:43 -0400

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('#typed-text', {
+      strings: ["este es mi portafolio","soy diseñadora frontend", "me gusta aprender"],
+      typeSpeed: 50,
+      backSpeed: 30,
+      loop: true
+    });
+  });
